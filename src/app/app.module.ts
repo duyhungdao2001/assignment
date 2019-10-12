6953128ea34739823cad6b0a85676be749215fe2
duyhung2000 <56204846+duyhung2000@users.noreply.github.com>
2019-10-13 01:02:39 +0700

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,34 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { RouterModule} from '@angular/router';
 import { AppComponent } from './app.component';
+import { TrangchuComponent } from './trangchu/trangchu.component';
+import { DangkyComponent } from './dangky/dangky.component';
+import { DangnhapComponent } from './dangnhap/dangnhap.component';
+import { LienheComponent } from './lienhe/lienhe.component';
+import { HoidapComponent } from './hoidap/hoidap.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TrangchuComponent,
+    DangkyComponent,
+    DangnhapComponent,
+    LienheComponent,
+    HoidapComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+      {path: '', component: TrangchuComponent},
+      {path:'trangchu', component: TrangchuComponent},
+      {path:'trangchu/:productld', component: TrangchuComponent},
+      {path: 'dangnhap', component: DangnhapComponent},
+      {path: 'lienhe', component: LienheComponent},
+      {path: 'dangky', component: DangkyComponent},
+      {path: 'hoidap', component: HoidapComponent},
+      {path:'**', redirectTo:'trangchu', pathMatch:"full" },
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
